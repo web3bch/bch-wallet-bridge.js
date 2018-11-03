65b106db637bb02099ee02df1c837a03edccd0d8
Yuiki <10101661+Yuiki@users.noreply.github.com>
2018-11-03 11:31:35 +0900

    add tslint

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@ import babel from "rollup-plugin-babel"
 import pkg from "./package.json"
 
 export default {
-    input: './src/index.ts',
+    input: './src/web3bch/index.ts',
     plugins: [
         babel({ extensions: ['.ts'], exclude: ['dist/**', 'node_modules/**'] }),
     ],
