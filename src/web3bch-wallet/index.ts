24c35dfc397306c15ba422c91c8c4b07739cae7a
Yuiki <10101661+Yuiki@users.noreply.github.com>
2018-11-03 13:57:45 +0900

    setup TypeDoc

--- src/web3bch-wallet/index.ts
+++ src/web3bch-wallet/index.ts
@@ -1,1 +1,28 @@
-console.log("Hello, World!")
+import IWeb3bchProvider from "../web3bch-provider"
+
+export type ChangeType = "receive" | "change"
+
+export default class Wallet {
+  constructor(
+    readonly provider: IWeb3bchProvider
+  ) {}
+
+  /**
+   * Returns the current wallet address.
+   * @example
+   * const address = await web3bch.wallet.getAddress(
+   *   "receive",
+   *   3,
+   *   "53212266f7994100e442f6dff10fbdb50a93121d25c196ce0597517d35d42e68"
+   * )
+   * console.log(address)
+   * > "bitcoincash:qrsy0xwugcajsqa99c9nf05pz7ndckj55ctlsztu2p"
+   * @param changeType The BIP44 change path type.
+   * @param index The BIP44 address_index path.
+   * @param dAppId The DApp ID. If no dAppId is set the default DApp ID will be set.
+   * @returns The current wallet address.
+   */
+  public getAddress = (changeType: ChangeType, index?: number, dAppId?: string): Promise<string> => {
+    return Promise.resolve("")
+  }
+}
--- src/web3bch/index.ts
+++ src/web3bch/index.ts
@@ -1,1 +1,7 @@
-console.log("Hello, World!")
+import IWeb3bchProvider from "../web3bch-provider"
+
+export default class Web3bch {
+  constructor(provider: IWeb3bchProvider) {
+    //
+  }
+}
